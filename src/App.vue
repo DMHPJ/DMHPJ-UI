5ed9df379bb89d2ec088bc43c5dd39f8953a2d05
chengqishe <809503475@qq.com>
2024-12-07 15:05:53 +0800

    更新展示页面

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,56 @@
 <template>
 	<div class="app">
-		<dm-card title="兜帽虎皮卷的卡片" shadow width="45%" height="50%">
+		<dm-card title="兜帽虎皮卷的卡片" shadow width="100%">
 			<template #header>
-				<div>兜帽虎皮卷的卡片标题</div>
+				<div>兜帽虎皮卷的按钮</div>
 			</template>
 			<div class="button-group">
-				<dm-button>兜帽虎皮卷的基础按钮</dm-button>
-				<dm-button plain>兜帽虎皮卷的简约按钮</dm-button>
-				<dm-button disabled>兜帽虎皮卷的禁用按钮</dm-button>
-				<dm-button plain disabled>兜帽虎皮卷的简约禁用按钮</dm-button>
+				<dm-button>基础按钮</dm-button>
+				<dm-button plain>简约按钮</dm-button>
+				<dm-button disabled>禁用按钮</dm-button>
+				<dm-button plain disabled>简约禁用按钮</dm-button>
 			</div>
-			<dm-table :data="testData">
-				<dm-table-column prop="code" label="编号" width="160">
-          <template #cell="{ row }">
+		</dm-card>
+		<dm-card title="兜帽虎皮卷的表格" shadow>
+			<dm-table style="margin-bottom: 1rem" :data="testData">
+				<dm-table-column prop="code" label="编号">
+					<template #cell="{ row }">
 						<div>{{ row.code }}</div>
 					</template>
-        </dm-table-column>
-				<dm-table-column prop="name" label="姓名" minWidth="160"></dm-table-column>
+				</dm-table-column>
+				<dm-table-column prop="name" label="姓名"></dm-table-column>
 				<dm-table-column prop="age" label="年龄"></dm-table-column>
-				<dm-table-column prop="remark" label="备注" width="200">
+				<dm-table-column prop="remark" label="备注">
 					<template #cell="{ row }">
 						<div>{{ row.remark }}</div>
 					</template>
 				</dm-table-column>
-      </dm-table>
-			<dm-checkbox-group v-model:modelValue="testRef.checkList">
-				<dm-checkbox label="复选框 A"></dm-checkbox>
-				<dm-checkbox label="复选框 B"></dm-checkbox>
-				<dm-checkbox label="复选框 C"></dm-checkbox>
+			</dm-table>
+		</dm-card>
+		<dm-card title="兜帽虎皮卷的复选框" shadow>
+			<dm-checkbox-group style="margin-bottom: 1rem" v-model:modelValue="testRef.checkList">
+				<dm-checkbox label="选中"></dm-checkbox>
+				<dm-checkbox label="未选中"></dm-checkbox>
 				<dm-checkbox label="禁用" disabled></dm-checkbox>
 				<dm-checkbox label="选中且禁用" disabled></dm-checkbox>
 			</dm-checkbox-group>
-				<dm-checkbox label="复选框 B" @change="testCheckbox"></dm-checkbox>
-			</dm-card>
+			<dm-checkbox-group style="margin-bottom: 1rem" v-model:modelValue="testRef.checkList">
+				<dm-checkbox border label="边框-选中" @change="testCheckbox"></dm-checkbox>
+				<dm-checkbox border label="边框-未选中"></dm-checkbox>
+				<dm-checkbox border label="边框-禁用" disabled></dm-checkbox>
+				<dm-checkbox border label="边框-选中且禁用" disabled></dm-checkbox>
+			</dm-checkbox-group>
+			<dm-checkbox-group style="margin-bottom: 1rem" v-model:modelValue="testRef.checkList">
+				<dm-checkbox-button label="按钮-选中" @change="testCheckbox"></dm-checkbox-button>
+				<dm-checkbox-button label="按钮-未选中"></dm-checkbox-button>
+				<dm-checkbox-button label="按钮-禁用" disabled></dm-checkbox-button>
+			</dm-checkbox-group>
+		</dm-card>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref, watch } from "vue";
 
 const testData = [
 	{
@@ -99,31 +112,32 @@ const testData = [
 ];
 const testRef = ref({
 	checked: false,
-	checkList: ['选中且禁用','复选框 A']
+	checkList: ["选中且禁用", "选中", "按钮-选中", "边框-选中", "边框-选中且禁用"],
 });
 
 const testCheckbox = (val: boolean, event: Event) => {
-	console.log('App Handle Checkbox Change', val, event);
+	console.log("App Handle Checkbox Change", val, event);
 };
 
-watch(() => testRef.value.checkList, (val) => {
-	console.log('App Watch CheckboxGroup', val);
-})
-
+watch(
+	() => testRef.value.checkList,
+	(val) => {
+		console.log("App Watch CheckboxGroup", val);
+	}
+);
 </script>
 
 <style scoped>
 .app {
-	display: flex;
-	align-items: center;
-	justify-content: center;
+	> div:not(:last-child) {
+		margin-bottom: 1rem;
+	}
 	height: 100vh;
 }
 
 .button-group {
-	margin-bottom: 1rem;
-	> div:not(:last-child) {
-		margin-right: 1rem;
-	}
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	gap: 0.75rem;
 }
 </style>
