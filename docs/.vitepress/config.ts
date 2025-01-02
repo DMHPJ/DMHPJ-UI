0c48cb8fed62032411ad066c6167068ea3d0d485
chengqishe <809503475@qq.com>
2025-01-02 18:28:38 +0800

    vitepress测试

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,22 +8,22 @@ export default defineConfig({
   base: base,
 	themeConfig: {
 		nav: [
-			{ text: "首页", link: `${base}` },
-			{ text: "说明文档", link: `${base}markdown-examples` },
+			{ text: "首页", link: "/" },
+			{ text: "说明文档", link: "/markdown-examples" },
 		],
 
 		sidebar: [
 			{
 				text: "开发指南",
 				items: [
-					{ text: "快速开始", link: `${base}/guide/getting-start` },
-					{ text: "查看说明文档", link: `${base}/api-examples` },
+					{ text: "快速开始", link: "/guide/getting-start" },
+					{ text: "查看说明文档", link: "/api-examples" },
 				],
 			},
       {
         text: "基础组件",
         items: [
-          { text: "Button 按钮", link: `${base}/guide/basic/button` },
+          { text: "Button 按钮", link: "/guide/basic/button" },
         ],
       }
 		],
