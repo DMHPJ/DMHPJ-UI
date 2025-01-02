c9314e19128c84a53c9e193afb0e545af216d33a
chengqishe <809503475@qq.com>
2025-01-02 18:24:13 +0800

    vitepress测试

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,27 +1,29 @@
 import { defineConfig } from "vitepress";
 
+const base = "/DMHPJ-UI/";
+
 export default defineConfig({
 	title: "🍰兜帽虎皮卷的UI组件库",
 	description: "This is DMHPJ's UI",
-  base: "/DMHPJ-UI/",
+  base: base,
 	themeConfig: {
 		nav: [
-			{ text: "首页", link: "/" },
-			{ text: "说明文档", link: "/markdown-examples" },
+			{ text: "首页", link: `${base}` },
+			{ text: "说明文档", link: `${base}markdown-examples` },
 		],
 
 		sidebar: [
 			{
 				text: "开发指南",
 				items: [
-					{ text: "快速开始", link: "/guide/getting-start" },
-					{ text: "查看说明文档", link: "/api-examples" },
+					{ text: "快速开始", link: `${base}/guide/getting-start` },
+					{ text: "查看说明文档", link: `${base}/api-examples` },
 				],
 			},
       {
         text: "基础组件",
         items: [
-          { text: "Button 按钮", link: "/guide/basic/button" },
+          { text: "Button 按钮", link: `${base}/guide/basic/button` },
         ],
       }
 		],
